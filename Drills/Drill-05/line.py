2d7c76d505cb75f0a69c738663531989635df59b
hyuntaek oh <jsoh1945@naver.com>
2019-10-07 19:39:38 +0900

    시작 지점에서 마우스가 찍은 점까지 캐릭터를 그리기
    2015182024 오현택 커밋횟수: 3회

--- Drills/Drill-05/line.py
+++ Drills/Drill-05/line.py
@@ -7,12 +7,11 @@ KPU_HEIGHT, KPU_WIDTH = 720, 1280
 
 
 def draw_line(p1, p2):
-    global Cx, Cy
     for i in range(0, 100 + 1, 5):
         t = i / 100
-        Cx = (1 - t) * p1[0] + t * p2[0]
-        Cy = (1 - t) * p1[1] + t * p2[1]
-
+        x = (1 - t) * p1[0] + t * p2[0]
+        y = (1 - t) * p1[1] + t * p2[1]
+        character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
     pass
 
 
@@ -20,6 +19,7 @@ def handle_events():
     global running, moving
     global Mx, My
     global Cx, Cy
+    global desX, desY
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
@@ -27,7 +27,9 @@ def handle_events():
         elif event.type == SDL_MOUSEMOTION:
             Mx, My = event.x, KPU_HEIGHT - 1 - event.y
         elif event.type == SDL_MOUSEBUTTONDOWN:
-            draw_line((Cx, Cy), (event.x - 25, 720 - event.y + 50))
+            moving = True
+            desX, desY = event.x - 25, 720 - event.y + 50
+            #draw_line((Cx, Cy), (event.x - 25, 720 - event.y + 50))
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
     pass
@@ -40,9 +42,10 @@ character = load_image("animation_sheet.png")
 cursor = load_image("hand_arrow.png")
 
 running = True
-moving = True
+moving = False
 Cx, Cy = KPU_WIDTH // 2, KPU_HEIGHT // 2
-#CxB4, CyB4 = 0, 0
+desX, desY = 0, 0
+CxO, CyO = KPU_WIDTH // 2, KPU_HEIGHT // 2
 Mx, My = KPU_WIDTH // 2, KPU_HEIGHT // 2
 frame = 0
 hide_cursor()
@@ -53,6 +56,9 @@ while running:
     kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
 
     character.clip_draw(frame * 100, 100 * 1, 100, 100, Cx, Cy)
+    if moving:
+        draw_line((Cx,Cy),(desX,desY))
+
     cursor.draw(Mx, My)
     update_canvas()
     frame = (frame + 1) % 8
